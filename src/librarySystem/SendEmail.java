95b3f58eba26accf9c37c2c625258a1ebe332465
dtowstar <kenca16358@gmail.com>
2018-10-30 17:50:52 +0800

    init

---
+++ src/librarySystem/SendEmail.java
@@ -0,0 +1,38 @@
+package librarySystem;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+public class SendEmail {
+	public static void send(Member i) {
+		// Get properties object
+		String from = "kencs16358@gmail.com";
+		String password = "ken12345678";
+		String to = i.getmemberemail();
+		String sub = "輸入標題";
+		String msg = "輸入內文";
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+		// get Session
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(from, password);
+			}
+		});
+		// compose message
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			message.setSubject(sub);
+			message.setText(msg);
+			// send message
+			Transport.send(message);
+			System.out.println("message sent successfully");
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
